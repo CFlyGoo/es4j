5e4b251673aa32f20ce89125104165c7dc4c724f
CFlyGoo <hpf.comail@gmail.com>
2018-08-20 23:34:58 +0800

    refactor: replace member sample class to support.sample classes

--- es4j-bus/src/test/java/com/apehat/es4j/util/graph/DirectedGraphTest.java
+++ es4j-bus/src/test/java/com/apehat/es4j/util/graph/DirectedGraphTest.java
@@ -20,8 +20,24 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
-import com.apehat.es4j.util.graph.DirectedGraph;
-import com.apehat.es4j.util.graph.Indicator;
+import com.apehat.es4j.support.sample.SampleClass1;
+import com.apehat.es4j.support.sample.SampleClass2;
+import com.apehat.es4j.support.sample.SampleClass3;
+import com.apehat.es4j.support.sample.SampleClass4;
+import com.apehat.es4j.support.sample.SampleClass5;
+import com.apehat.es4j.support.sample.SampleClass6;
+import com.apehat.es4j.support.sample.SampleClass7;
+import com.apehat.es4j.support.sample.SampleClass8;
+import com.apehat.es4j.support.sample.SampleMiddleClass1;
+import com.apehat.es4j.support.sample.SampleMiddleClass2;
+import com.apehat.es4j.support.sample.SampleMiddleClass3;
+import com.apehat.es4j.support.sample.SampleMiddleClass4;
+import com.apehat.es4j.support.sample.SampleMiddleInterface1;
+import com.apehat.es4j.support.sample.SampleMiddleInterface2;
+import com.apehat.es4j.support.sample.SampleMiddleInterface3;
+import com.apehat.es4j.support.sample.SampleSuperClass;
+import com.apehat.es4j.support.sample.SampleSuperInterface1;
+import com.apehat.es4j.support.sample.SampleSuperInterface2;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -35,7 +51,6 @@ import org.testng.annotations.Test;
  * @author hanpengfei
  * @since 1.0
  */
-@SuppressWarnings("WeakerAccess")
 public class DirectedGraphTest {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -49,13 +64,12 @@ public class DirectedGraphTest {
 
     static {
         Class<?>[] sampleArray = new Class[]{
-            MiddleInterface2.class, MiddleClass1.class, MiddleClass2.class,
-            MiddleClass3.class, SuperInterface1.class, SuperInterface2.class,
-            Class3.class, Class4.class, Class5.class, Class6.class, Class7.class,
-            Class9.class, Class10.class, Class11.class, Class12.class, Class13.class,
-            Class8.class, SuperClass1.class, MiddleInterface3.class, Class1.class,
-            Class2.class, Class14.class, Class15.class, Class16.class, MiddleClass4.class,
-            MiddleInterface1.class
+            SampleMiddleInterface2.class, SampleMiddleClass1.class, SampleMiddleClass2.class,
+            SampleMiddleClass3.class, SampleSuperInterface1.class, SampleSuperInterface2.class,
+            SampleClass3.class, SampleClass4.class, SampleClass5.class, SampleClass6.class,
+            SampleClass7.class, SampleClass8.class, SampleSuperClass.class,
+            SampleMiddleInterface3.class, SampleClass1.class, SampleClass2.class,
+            SampleMiddleClass4.class, SampleMiddleInterface1.class
         };
         SAMPLE = new HashSet<>(Arrays.asList(sampleArray));
         init();
@@ -96,70 +110,11 @@ public class DirectedGraphTest {
         return supers;
     }
 
-    private interface SuperInterface1 {}
-
-    private interface SuperInterface2 {}
-
-    private interface MiddleInterface1 extends SuperInterface1 {}
-
-    private interface MiddleInterface2 extends SuperInterface2 {}
-
-    private interface MiddleInterface3 extends SuperInterface1, SuperInterface2 {}
-
-    private static class SuperClass1 {}
-
-    private static class MiddleClass1 extends SuperClass1 {}
-
-    private static class MiddleClass2 extends SuperClass1 implements SuperInterface1 {}
-
-    private static class MiddleClass3 extends SuperClass1 implements SuperInterface2 {}
-
-    private static class MiddleClass4 extends SuperClass1 implements SuperInterface1,
-        SuperInterface2 {}
-
-    private static class Class1 extends MiddleClass1 {}
-
-    private static class Class2 extends MiddleClass1 implements SuperInterface2 {}
-
-    private static class Class3 extends MiddleClass1 implements MiddleInterface1 {}
-
-    private static class Class4 extends MiddleClass1 implements MiddleInterface2 {}
-
-    private static class Class5 extends MiddleClass1 implements MiddleInterface3 {}
-
-    private static class Class6 extends MiddleClass1 implements MiddleInterface1,
-        MiddleInterface2 {}
-
-    private static class Class7 extends MiddleClass1 implements MiddleInterface1,
-        MiddleInterface3 {}
-
-    private static class Class8 extends MiddleClass1 implements MiddleInterface1, MiddleInterface2,
-        MiddleInterface3 {}
-
-    private static class Class9 extends MiddleClass1 {}
-
-    private static class Class10 extends MiddleClass1 implements SuperInterface2 {}
-
-    private static class Class11 extends MiddleClass1 implements MiddleInterface1 {}
-
-    private static class Class12 extends MiddleClass1 implements MiddleInterface2 {}
-
-    private static class Class13 extends MiddleClass1 implements MiddleInterface3 {}
-
-    private static class Class14 extends MiddleClass1 implements MiddleInterface1,
-        MiddleInterface2 {}
-
-    private static class Class15 extends MiddleClass1 implements MiddleInterface1,
-        MiddleInterface3 {}
-
-    private static class Class16 extends MiddleClass1 implements MiddleInterface1, MiddleInterface2,
-        MiddleInterface3 {}
-
     public static Set<Class<?>> getSample() {
         return SAMPLE;
     }
 
-    public static Indicator<Class<?>> getIndicator() {
+    protected static Indicator<Class<?>> getIndicator() {
         return INDICATOR;
     }
 
@@ -172,7 +127,7 @@ public class DirectedGraphTest {
 
     @Test
     public void testGetLayer() {
-        assertEquals(2, directedGraph.getLayer(MiddleInterface3.class));
+        assertEquals(2, directedGraph.getLayer(SampleMiddleInterface3.class));
     }
 
     @Test
@@ -221,12 +176,12 @@ public class DirectedGraphTest {
         }
     }
 
-
     @Test
     public void testGetIn() {
         Class<?>[] array = new Class[]{
-            MiddleClass1.class, MiddleClass2.class, MiddleClass3.class, MiddleClass4.class,
-            MiddleInterface1.class, MiddleInterface2.class, MiddleInterface3.class
+            SampleMiddleClass1.class, SampleMiddleClass2.class, SampleMiddleClass3.class,
+            SampleMiddleClass4.class,
+            SampleMiddleInterface1.class, SampleMiddleInterface2.class, SampleMiddleInterface3.class
         };
         assertEquals(directedGraph.getIn(2), new HashSet<>(Arrays.asList(array)));
     }
@@ -244,7 +199,7 @@ public class DirectedGraphTest {
     @Test
     public void testGetTop() {
         Class<?>[] topClasses = new Class[]{
-            SuperInterface1.class, SuperInterface2.class, SuperClass1.class
+            SampleSuperInterface1.class, SampleSuperInterface2.class, SampleSuperClass.class
         };
         assertEquals(directedGraph.getTop(), new HashSet<>(Arrays.asList(topClasses)));
     }
