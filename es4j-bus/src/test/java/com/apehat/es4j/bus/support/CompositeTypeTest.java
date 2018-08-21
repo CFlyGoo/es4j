a630842b5f74738df528814455ab73fbf8646eb0
CFlyGoo <hpf.comail@gmail.com>
2018-08-21 23:31:04 +0800

    test: test by random

--- es4j-bus/src/test/java/com/apehat/es4j/bus/support/CompositeTypeTest.java
+++ es4j-bus/src/test/java/com/apehat/es4j/bus/support/CompositeTypeTest.java
@@ -23,8 +23,26 @@ import com.apehat.es4j.bus.Type;
 import com.apehat.es4j.support.sample.SampleClass1;
 import com.apehat.es4j.support.sample.SampleClass2;
 import com.apehat.es4j.support.sample.SampleClass3;
+import com.apehat.es4j.support.sample.SampleClass4;
+import com.apehat.es4j.support.sample.SampleClass5;
+import com.apehat.es4j.support.sample.SampleClass6;
+import com.apehat.es4j.support.sample.SampleClass7;
+import com.apehat.es4j.support.sample.SampleClass8;
 import com.apehat.es4j.support.sample.SampleMiddleClass1;
+import com.apehat.es4j.support.sample.SampleMiddleClass2;
+import com.apehat.es4j.support.sample.SampleMiddleClass3;
+import com.apehat.es4j.support.sample.SampleMiddleClass4;
+import com.apehat.es4j.support.sample.SampleMiddleInterface1;
+import com.apehat.es4j.support.sample.SampleMiddleInterface2;
+import com.apehat.es4j.support.sample.SampleMiddleInterface3;
 import com.apehat.es4j.support.sample.SampleSuperClass;
+import com.apehat.es4j.support.sample.SampleSuperInterface1;
+import com.apehat.es4j.support.sample.SampleSuperInterface2;
+import java.util.Arrays;
+import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
@@ -33,38 +51,84 @@ import org.testng.annotations.Test;
  */
 public class CompositeTypeTest {
 
-    private final CompositeType type;
-    private final Class<?>[] initClasses;
+    private static final Logger LOGGER = LoggerFactory.getLogger(CompositeTypeTest.class);
 
-    public CompositeTypeTest() {
-        initClasses = new Class[]{
-            SampleClass1.class, SampleClass2.class, SampleMiddleClass1.class
+    private Type type;
+    private Type typeWithRemoved;
+    private Class<?>[] initSamples;
+    private Class<?>[] removedSamples;
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @BeforeMethod
+    public void setUp() {
+        // all test sample
+        Class<?>[] samples = new Class[]{
+            SampleMiddleInterface2.class, SampleMiddleClass1.class, SampleMiddleClass2.class,
+            SampleMiddleClass3.class, SampleSuperInterface1.class, SampleSuperInterface2.class,
+            SampleClass3.class, SampleClass4.class, SampleClass5.class, SampleClass6.class,
+            SampleClass7.class, SampleClass8.class, SampleSuperClass.class,
+            SampleMiddleInterface3.class, SampleClass1.class, SampleClass2.class,
+            SampleMiddleClass4.class, SampleMiddleInterface1.class
         };
-        type = new CompositeType(initClasses);
 
-        type.remove(SampleClass3.class);
-    }
-
-    @Test
-    public void testAdd() {
-        assertFalse(type.isAssignableFrom(SampleSuperClass.class));
-        Type newType = type.add(SampleSuperClass.class);
-        assertFalse(type.isAssignableFrom(SampleSuperClass.class));
-        assertTrue(newType.isAssignableFrom(SampleSuperClass.class));
-    }
-
-    @Test
-    public void testRemove() {
-        assertTrue(type.isAssignableFrom(SampleClass1.class));
-        Type newType = type.remove(SampleClass1.class);
-        assertTrue(type.isAssignableFrom(SampleClass1.class));
-        assertFalse(newType.isAssignableFrom(SampleClass1.class));
-    }
-
-    @Test
-    public void testIsAssignableFrom() {
-        for (Class<?> cls : initClasses) {
-            assertTrue(type.isAssignableFrom(cls));
+        // init sample
+        int initCount;
+        while ((initCount = new Random().nextInt(samples.length)) < samples.length * 2 / 3) {
         }
+        this.initSamples = new Class[initCount];
+        for (int i = 0; i < initCount; i++) {
+            initSamples[i] = samples[new Random().nextInt(samples.length)];
+        }
+
+        // remove sample
+        int removeCount;
+        while ((removeCount = new Random().nextInt(initSamples.length / 2)) == 0) {
+        }
+        this.removedSamples = new Class[removeCount];
+        for (int i = 0; i < removeCount; i++) {
+            removedSamples[i] = samples[new Random().nextInt(samples.length)];
+        }
+
+        // init type
+        LOGGER.debug("Construct CompositeType with {}", Arrays.toString(initSamples));
+        this.type = new CompositeType(initSamples);
+        LOGGER.debug("Remove {} from {}", Arrays.toString(removedSamples), type);
+        Type temp = type;
+        for (Class<?> removedSample : removedSamples) {
+            LOGGER.debug("Will remove singleton type {} from {}", removedSample, temp);
+            temp = temp.remove(removedSample);
+        }
+        typeWithRemoved = temp;
+        LOGGER.debug("typeWithRemoved is {}", typeWithRemoved);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testConstructWithNull() {
+        new CompositeType();
+    }
+
+    @Test(invocationCount = 10)
+    public void testAdd() {
+        final Class<?> nonAssignableType =
+            removedSamples[new Random().nextInt(removedSamples.length)];
+        LOGGER.debug("Start test add by {} from {}", nonAssignableType, typeWithRemoved);
+        assertFalse(typeWithRemoved.isAssignableFrom(nonAssignableType));
+        final Type newType = typeWithRemoved.add(nonAssignableType);
+        LOGGER.debug("New type {}", newType);
+        assertTrue(newType.isAssignableFrom(nonAssignableType));
+        LOGGER.debug("After add {}", typeWithRemoved);
+        assertFalse(typeWithRemoved.isAssignableFrom(nonAssignableType));
+    }
+
+    @Test(invocationCount = 10)
+    public void testRemove() {
+        final Class<?> assignableType = initSamples[new Random().nextInt(initSamples.length)];
+        LOGGER.debug("Start test remove by {} from {}", assignableType, type);
+        assertTrue(type.isAssignableFrom(assignableType));
+        final Type newType = type.remove(assignableType);
+        LOGGER.debug("New type {}", newType);
+        assertFalse(newType.isAssignableFrom(assignableType));
+        LOGGER.debug("After remove {}", type);
+        assertTrue(type.isAssignableFrom(assignableType));
     }
 }
