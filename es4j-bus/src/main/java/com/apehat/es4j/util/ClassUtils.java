7e9dd929e5db95e912f9a7d81f135649ccfc0bf6
CFlyGoo <hpf.comail@gmail.com>
2018-08-21 14:56:11 +0800

    feat: new method to get superclasses and interfaces

--- es4j-bus/src/main/java/com/apehat/es4j/util/ClassUtils.java
+++ es4j-bus/src/main/java/com/apehat/es4j/util/ClassUtils.java
@@ -16,6 +16,11 @@
 
 package com.apehat.es4j.util;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * @author hanpengfei
  * @since 1.0
@@ -29,5 +34,25 @@ public final class ClassUtils {
     public static <T> Class<T> getParameterizedClass(T object) {
         //noinspection unchecked - safe
         return (Class<T>) object.getClass();
+    }
+
+    public static Set<Class<?>> getAllSuperclassesAndInterfaces(Class<?> cls) {
+        final Set<Class<?>> supers = getSuperclassAndInterfaces(cls);
+        final Set<Class<?>> temp = new HashSet<>();
+        for (Class<?> aSuper : supers) {
+            temp.addAll(getAllSuperclassesAndInterfaces(aSuper));
+        }
+        supers.addAll(temp);
+        return supers;
+    }
+
+    public static Set<Class<?>> getSuperclassAndInterfaces(Class<?> cls) {
+        Objects.requireNonNull(cls, "Class must not be null");
+        final Set<Class<?>> supers = new HashSet<>();
+        if (cls != Object.class && !cls.isInterface()) {
+            supers.add(cls.getSuperclass());
+        }
+        supers.addAll(Arrays.asList((cls.getInterfaces())));
+        return supers;
     }
 }
