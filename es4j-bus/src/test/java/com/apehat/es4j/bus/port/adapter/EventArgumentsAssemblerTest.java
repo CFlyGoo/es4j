95d4821dd454d244395c15bb4a15933077088bb1
CFlyGoo <hpf.comail@gmail.com>
2018-08-28 20:24:24 +0800

    test: EventArgumentsAssembler

---
+++ es4j-bus/src/test/java/com/apehat/es4j/bus/port/adapter/EventArgumentsAssemblerTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.apehat.es4j.bus.port.adapter;
+
+import static org.testng.Assert.assertEquals;
+
+import com.apehat.es4j.bus.DomainRegistry;
+import com.apehat.es4j.bus.event.Event;
+import com.apehat.es4j.bus.event.EventFixtureProvider;
+import com.apehat.es4j.support.TestDataProvider;
+import com.apehat.es4j.support.UserId;
+import com.apehat.es4j.support.UserRegistered;
+import java.lang.reflect.Method;
+import java.util.Date;
+import org.testng.annotations.Test;
+
+/**
+ * @author hanpengfei
+ * @since 1.0
+ */
+public class EventArgumentsAssemblerTest {
+
+    private final Event event;
+    private final UserRegistered prototype;
+    private final EventArgumentsAssembler assembler;
+
+    public EventArgumentsAssemblerTest() {
+        this.prototype = TestDataProvider.userRegisteredFixture();
+        this.event = EventFixtureProvider.newEventFixture(prototype);
+        this.assembler = new EventArgumentsAssembler(
+            DomainRegistry.parameterNameDiscoverer(), new EventArgumentExtractor());
+    }
+
+    @Test
+    public void testAssemble() throws Exception {
+        Method method = EventArgumentsAssemblerTest.class.getDeclaredMethod(
+            "testGetSingleLayerArguments", long.class, Object.class, Class.class, String.class,
+            UserId.class, String.class, Date.class);
+        Object[] arguments = assembler.assemble(method, event);
+        //noinspection JavaReflectionInvocation
+        method.invoke(this, arguments);
+    }
+
+    private void testGetSingleLayerArguments(long occurredOn, Object event,
+        Class<?> type, String source, UserId userId, String username, Date registerOn) {
+        assertEquals(this.event.occurredOn(), occurredOn);
+        assertEquals(this.event.prototype(), event);
+        assertEquals(this.event.type(), type);
+        assertEquals(this.event.source(), source);
+        assertEquals(prototype.getUserId(), userId);
+        assertEquals(prototype.getUsername(), username);
+        assertEquals(prototype.getRegisterOn(), registerOn);
+    }
+}
