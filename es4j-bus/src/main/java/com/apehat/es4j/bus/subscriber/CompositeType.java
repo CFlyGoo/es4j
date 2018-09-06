fbfc2d9340cf86e59078b049575d167533aa1610
CFlyGoo <hpf.comail@gmail.com>
2018-09-06 21:40:00 +0800

    refactor: upgrade import

--- es4j-bus/src/main/java/com/apehat/es4j/bus/subscriber/CompositeType.java
+++ es4j-bus/src/main/java/com/apehat/es4j/bus/subscriber/CompositeType.java
@@ -16,9 +16,9 @@
 
 package com.apehat.es4j.bus.subscriber;
 
-import com.apehat.util.AbstractClassItem;
-import com.apehat.util.ClassItemCombiner;
-import com.apehat.util.Item;
+import com.apehat.AbstractClassItem;
+import com.apehat.ClassItemCombiner;
+import com.apehat.Item;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
