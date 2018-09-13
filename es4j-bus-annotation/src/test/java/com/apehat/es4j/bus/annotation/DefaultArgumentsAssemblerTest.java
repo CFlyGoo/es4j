8b26f5339bf86ebdb76222da121e2c186ef79c2f
CFlyGoo <hpf.comail@gmail.com>
2018-09-13 18:00:27 +0800

    refactor: use EventFixtureProvider and EventHandleMethodProvider

--- es4j-bus-annotation/src/test/java/com/apehat/es4j/bus/annotation/DefaultArgumentsAssemblerTest.java
+++ es4j-bus-annotation/src/test/java/com/apehat/es4j/bus/annotation/DefaultArgumentsAssemblerTest.java
@@ -18,20 +18,17 @@ package com.apehat.es4j.bus.annotation;
 
 import static org.testng.Assert.assertEquals;
 
-import com.apehat.Value;
 import com.apehat.argument.binding.ArgumentsAssembler;
 import com.apehat.argument.binding.DefaultArgumentsAssembler;
 import com.apehat.argument.binding.alias.DefaultParameterAliasDiscoverer;
-import com.apehat.argument.binding.support.DefaultArgumentAdapter;
-import com.apehat.argument.binding.support.PrioritizedArgumentAdapter;
-import com.apehat.clone.DefaultCloningService;
+import com.apehat.es4j.bus.event.Event;
+import com.apehat.es4j.bus.event.EventFixtureProvider;
+import com.apehat.es4j.bus.port.adapter.EventArgumentAdapterProvider;
 import com.apehat.support.TestDataProvider;
 import com.apehat.support.UserId;
 import com.apehat.support.UserRegistered;
 import java.lang.reflect.Method;
 import java.util.Date;
-import java.util.Objects;
-import java.util.UUID;
 import org.testng.annotations.Test;
 
 /**
@@ -46,29 +43,25 @@ public class DefaultArgumentsAssemblerTest {
 
     public DefaultArgumentsAssemblerTest() {
         this.prototype = TestDataProvider.userRegisteredFixture();
-        this.event = new Event(
-            System.currentTimeMillis(),
-            this.prototype,
-            UUID.randomUUID().toString());
+        this.event = EventFixtureProvider.newEventFixture(this.prototype);
         this.assembler = new DefaultArgumentsAssembler<>(
             new DefaultParameterAliasDiscoverer(),
-            new EventArgumentAdapter());
+            EventArgumentAdapterProvider.eventArgumentAdapterFixture());
     }
 
     @Test
     public void testAssemble() throws Exception {
-        Method method = DefaultArgumentsAssemblerTest.class
-            .getDeclaredMethod("testGetSingleLayerArguments",
-                long.class, Object.class, Class.class,
-                String.class, UserId.class, String.class, Date.class);
+        Method method = getClass().getDeclaredMethod(
+            "testGetSingleLayerArguments",
+            long.class, Object.class, Class.class,
+            String.class, UserId.class, String.class, Date.class);
         Object[] arguments = assembler.assemble(method, event);
         //noinspection JavaReflectionInvocation
         method.invoke(this, arguments);
     }
 
-    private void testGetSingleLayerArguments(
-        long occurredOn, Object event, Class<?> type,
-        String source, UserId userId, String username, Date registerOn) {
+    private void testGetSingleLayerArguments(long occurredOn, Object event,
+        Class<?> type, String source, UserId userId, String username, Date registerOn) {
         assertEquals(this.event.occurredOn(), occurredOn);
         assertEquals(this.event.prototype(), event);
         assertEquals(this.event.type(), type);
@@ -76,87 +69,5 @@ public class DefaultArgumentsAssemblerTest {
         assertEquals(prototype.getUserId(), userId);
         assertEquals(prototype.getUsername(), username);
         assertEquals(prototype.getRegisterOn(), registerOn);
-    }
-
-    private final class EventArgumentAdapter extends PrioritizedArgumentAdapter {
-
-        EventArgumentAdapter() {
-            registerAdapter(new AnnotatedMethodAdapter());
-            registerAdapter(new AnnotateFieldAdapter());
-            registerAdapter(new DefaultArgumentAdapter());
-        }
-
-        @Override
-        public Value<?> adapt(String alias, Object prototype) {
-            Value<?> value = super.adapt(alias, prototype);
-            if (value != null) {
-                return value;
-            }
-            return super.adapt(alias, ((Event) prototype).prototype());
-        }
-    }
-
-    public final class Event {
-
-        private final long occurredOn;
-        private final Object prototype;
-        private final String source;
-
-        Event(long occurredOn, Object prototype, String source) {
-            this.prototype = new DefaultCloningService().deepClone(
-                Objects.requireNonNull(prototype, "Event prototype must not be null"));
-            this.occurredOn = occurredOn;
-            this.source = source;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Event that = (Event) o;
-            return occurredOn == that.occurredOn &&
-                Objects.equals(prototype, that.prototype) &&
-                Objects.equals(source, that.source);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = 253;
-            result = 31 * result + Objects.hash(occurredOn);
-            result = 31 * result + Objects.hash(prototype);
-            result = 31 * result + Objects.hash(source);
-            return result;
-        }
-
-        @Override
-        public String toString() {
-            return "Event{" +
-                "type=" + type() +
-                ", occurredOn=" + occurredOn +
-                ", prototype=" + prototype +
-                ", source='" + source + '\'' +
-                '}';
-        }
-
-        long occurredOn() {
-            return occurredOn;
-        }
-
-        @Alias("event")
-        Object prototype() {
-            return new DefaultCloningService().deepClone(prototype);
-        }
-
-        String source() {
-            return source;
-        }
-
-        Class<?> type() {
-            return prototype.getClass();
-        }
     }
 }
